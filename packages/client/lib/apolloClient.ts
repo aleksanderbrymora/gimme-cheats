607c3d452a438697058d7bb645ed21305c32b5bb
aleksanderbrymora <aleksander.australia@gmail.com>
2020-12-28 16:38:21 +0100

    seams to work need to enable playground

--- packages/client/lib/apolloClient.ts
+++ packages/client/lib/apolloClient.ts
@@ -12,8 +12,8 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined', // set to true for SSR
     link: new HttpLink({
-      // uri: process.env.GQL_URL
-      uri: 'http://localhost:4000/graphql',
+      uri: process.env.GQL_URL,
+      // uri: 'http://localhost:4000/graphql',
     }),
     cache: new InMemoryCache(),
   });
